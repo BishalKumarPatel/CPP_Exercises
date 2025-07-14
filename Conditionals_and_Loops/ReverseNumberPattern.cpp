1e973e5b107defbd5bb4513580f81802efaa51a0
Bishal Patel <bishalkumarpatel267@gmail.com>
2025-07-14 13:27:22 +0530

    Program to display Reverse Number Pattern

---
+++ Conditionals_and_Loops/ReverseNumberPattern.cpp
@@ -0,0 +1,43 @@
+/*
+Problem statement
+Print the following pattern for the given N number of rows.
+
+Pattern for N = 4
+1
+21
+321
+4321
+
+Constraints
+0 <= N <= 50
+Sample Input 1:
+5
+Sample Output 1:
+1
+21
+321
+4321
+54321
+Sample Input 2:
+6
+Sample Output 2:
+1
+21
+321
+4321
+54321
+654321
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int j=i; j>=1; j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
